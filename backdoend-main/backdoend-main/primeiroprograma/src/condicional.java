6d42f29ae0b105e42f2755ea4285e1bc0b7c7358
antonimateus <127150996+antonimateus@users.noreply.github.com>
2024-09-24 09:09:15 -0300

    Add files via upload

---
+++ backdoend-main/backdoend-main/primeiroprograma/src/condicional.java
@@ -0,0 +1,12 @@
+public class condicional {
+    public static void main (String[] args) {
+        int anodelancamento = 2022;
+        boolean incluindonoplano = true;
+        double notadofilme = 8.1;
+        if (anodelancamento >= 2022) {
+            System.out.println("lancamento que os clientes estao curtindo!");}
+        else {
+            System.out.println("filme retro que vale a pena assistir!");
+        }
+    }
+}
