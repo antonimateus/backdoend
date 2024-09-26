a1e3732d027762a76ec863ac99c92f2a4b6d579a
antonimateus <127150996+antonimateus@users.noreply.github.com>
2024-09-26 17:26:50 -0300

    Add files via upload

---
+++ primeiroprograma/src/condicional.java
@@ -0,0 +1,19 @@
+public class condicional {
+    public static void main (String[] args) {
+        int anodelancamento = 2022;
+        boolean incluindonoplano = true;
+        double notadofilme = 8.1;
+        String tipoPlano = "plus";
+        if (anodelancamento >= 2022) {
+            System.out.println("lancamento que os clientes estao curtindo!");}
+        else {
+            System.out.println("filme retro que vale a pena assistir!");
+        }
+
+        if (incluidoNoplano == true && tipoPlano.equals("plus")) {
+            System.out.println("filme liberado");
+        } else {
+            System.out.println("Deve pagar a locação");
+        }
+    }
+}
